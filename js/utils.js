e59ac9861be09d6276a7e0ea538fb63bc20ed253
Manolo Bevia <manolo.bevia@cornell.edu>
2016-12-07 15:13:27 -0500

    Update js files Updates theme files

--- js/utils.js
+++ js/utils.js
@@ -206,53 +206,6 @@ $( document ).ready(function() {
         });
 
       
-      //Add active class to parent link in megamenus
-
-      
-
-
-      $('.region-highlights .view-content').slick({
-        
-          dots: true,
-          infinite: false,
-          speed: 300,
-          slidesToShow: 3,
-          slidesToScroll: 3,
-
-          responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: false,
-                dots: true
-              }
-            },
-            {
-              breakpoint: 800,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 640,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-        });
-
-
-
-
-
 
 });
 
